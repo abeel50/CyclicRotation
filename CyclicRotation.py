eeda2096229a18882adc639fa1f245a837d471ec
abeel50 <abeelmuhammad50@gmail.com>
2022-06-13 15:24:08 +0500

    100%

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -1,9 +1,9 @@
 def solution(A, K):
     # write your code in Python 3.6
-    if len(A) == K or A.count(A[0]) == len(A):
+    if len(A) == 0 or K == 0 or len(A) == K or A.count(A[0]) == len(A):
         return A
     else:
         new_A = [0] * len(A)
-        for i in range(0,len(A)):
-            new_A[(i+(K % len(A)))%len(A)] = A[i]
+        for i in range(0, len(A)):
+            new_A[( i + (K % len(A))) % len(A)] = A[i]
         return new_A  
